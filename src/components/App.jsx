cdabd2f8005698dcd790426bfbff40fdb3bc967c
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-08-06 09:33:48 +0300

    st

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,20 @@ export class App extends Component {
     contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   handleSubmit = obj => {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, { ...obj }],
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,83 +1,72 @@
-import React from 'react';
+// import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { nanoid } from 'nanoid';
 import styles from './Form.module.css';
 
-class Form extends React.Component {
-  state = {
-    name: '',
-    number: '',
+export default function Form({ handleSubmitForm, contacts }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
-
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmitForm = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, number } = this.state;
-    const { handleSubmit, contacts } = this.props;
     const id = nanoid();
-    const obj = {
-      id,
-      name,
-      number,
-    };
-
     const check = contacts.find(
       el => el.name.toLowerCase() === name.toLowerCase()
     );
     if (!check) {
-      handleSubmit(obj);
-      this.reset();
+      handleSubmitForm({ id, name, number });
+      setName('');
+      setNumber('');
     } else {
       alert('Error', name + 'is already in contacts', 'Okay');
     }
   };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmitForm} className={styles.form}>
-        <label className={styles.label}>
-          Name
-          <input
-            className={styles.input}
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange}
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className={styles.label}>
-          Number
-          <input
-            className={styles.input}
-            type="tel"
-            value={this.state.number}
-            onChange={this.handleChange}
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button type="submit">add contact</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label className={styles.label}>
+        Name
+        <input
+          className={styles.input}
+          type="text"
+          value={name}
+          onChange={handleChange}
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label className={styles.label}>
+        Number
+        <input
+          className={styles.input}
+          type="tel"
+          value={number}
+          onChange={handleChange}
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button type="submit">add contact</button>
+    </form>
+  );
 }
-export default Form;
 
 Form.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
